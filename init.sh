4922a88df52b4c97da609d4b166d2aa260358e35
Nathan Smoot <n@nathansmoot.com>
2015-03-29 12:58:08 -0400

    link psql history to /dev/null on init

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ for f in `ls -A .dotfiles/ | grep -v 'init.sh'`; do
   ln -s .dotfiles/$f
 done
 
-for f in .bash_history .mysql_history .sqlite_history; do
+for f in .bash_history .mysql_history .psql_history .sqlite_history; do
   rm -f ~/$f
   ln -s /dev/null ~/$f
 done
